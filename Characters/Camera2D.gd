edcd0c96874d4bdf97bb2e3efd78cc1c0f0b3e6f
DripJesus <trentchaplinb@gmail.com>
2023-10-18 17:19:51 +1100

    Camera Variable Name Fix

--- Characters/Camera2D.gd
+++ Characters/Camera2D.gd
@@ -1,11 +1,11 @@
 extends Camera2D
 
 @onready var player = $".."
-var dash_lag : float = 5
-var walk_lag : float = 2
+var dash_lag : float = 2
+var walk_lag : float = 6
 
 func _physics_process(_delta):
 	if player.isDashing:
-		position_smoothing_speed = walk_lag
+		position_smoothing_speed = dash_lag
 	if !player.isDashing:
-		position_smoothing_speed = lerp(position_smoothing_speed, dash_lag, 0.5)
+		position_smoothing_speed = walk_lag
